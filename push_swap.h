98dfad64aa2044526af98bc096eaa919cd2fd4c1
uzanchi <ugozanchi5@gmail.com>
2024-06-17 13:16:24 +0200

    .h

--- push_swap.h
+++ push_swap.h
@@ -1,8 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uzanchi <uzanchi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/17 13:03:51 by uzanchi           #+#    #+#             */
+/*   Updated: 2024/06/17 13:03:55 by uzanchi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
 # include "../libft/libft.h"
 
 # define ERROR_MESSAGE "Error\n"
+
+typedef struct s_stack_node
+{
+	int	nbr;
+	int	index;
+	int	push_cost;
+	bool	above_median;
+	bool	cheapest;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;	
+}	t_stack_node;
 
 #endif
